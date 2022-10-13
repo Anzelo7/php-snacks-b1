ccc745a8d506543f6ba0f89de2faaa0741ea9a0e
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-10-13 15:53:25 +0200

    costruzione array

---
+++ index.php
@@ -0,0 +1,26 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60  -->
+
+<?php
+  $andata = [ 
+    [ 
+      "casa" => "Carpegna Prosciutto Pesaro",
+      "puntiCasa" => 82,
+      "ospite" => "Dolomiti Energia Trentino",
+      "puntiOspite" => 106,
+    ],
+    [
+      "casa" => "EA7 Milano",
+      "puntiCasa" => 59,
+      "ospite" => "Happy Casa Brindisi",
+      "puntiOspite" => 74,
+    ],
+    [
+      "casa" => "Tezenis Verona",
+      "puntiCasa" => 75,
+      "ospite" => "Banco di Sardegna Sassari",
+      "puntiOspite" => 70,
+    ]
+  ];
